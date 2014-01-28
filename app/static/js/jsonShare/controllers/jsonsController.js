193850698fb6ef8bee578823f7e6c34449f6cb26
Sheetal, the invincible <sheetaluk@spotify.com>
2014-01-28 18:05:59 -0500

    setup
    Change-Id: I78a26a3ffefaddd627efbda8a2041b30b3c3263d

---
+++ app/static/js/jsonShare/controllers/jsonsController.js
@@ -0,0 +1,11 @@
+(function() {
+  angular.module('JsonShare.controllers')
+  .controller('JsonsController', [
+    '$scope',
+    '$routeParams',
+    '$window',
+    function($scope, $routeParams, $window) {
+      'use strict';
+    }
+  ]);
+}());
